e2f9ab473af7cc218732564c945b116c1673058c
delozier53 <joshuamdelozier@gmail.com>
2025-08-10 01:02:51 -0500

    Fix WebSocket SSL connection error

--- server/db.ts
+++ server/db.ts
@@ -4,6 +4,7 @@ import ws from "ws";
 import * as schema from "@shared/schema";
 
 neonConfig.webSocketConstructor = ws;
+neonConfig.connection = { rejectUnauthorized: false };
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
